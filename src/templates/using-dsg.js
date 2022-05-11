68118c28ac24f72ee8c484258154884caec52d98
Anna <teesannalv@gmail.com>
2022-05-11 14:39:39 +0300

    Boilerplate configured. DSG error fixed.

---
+++ src/templates/using-dsg.js
@@ -0,0 +1,21 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+const UsingDSG = () => (
+  <section>
+    <h1>
+      Hello from a <b>DSG Page</b>
+    </h1>
+    <p>This page is not created until requested by a user.</p>
+    <p>
+      To learn more, head over to our{" "}
+      <a href="https://www.gatsbyjs.com/docs/reference/rendering-options/deferred-static-generation/">
+        documentation about Deferred Static Generation
+      </a>
+      .
+    </p>
+    <Link to="/">Go back to the homepage</Link>
+  </section>
+)
+
+export default UsingDSG
