532a540a673c89fdb700fbe39ad7d39d64754d02
Anna <teesannalv@gmail.com>
2022-05-13 13:49:22 +0300

    Basic projects page setup.

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,13 +1,29 @@
 import React from 'react'
 import Layout from '../../components/Layout'
+import Wrapper from '../../components/Wrapper'
+import * as styles from '../../styles/projects.module.scss'
 
 export default function Projects() {
   return (
     <Layout>
-      <div>
-          <h1>Projects</h1>
-          <h2>All of my projects</h2>
-      </div>
+      <Wrapper>
+        <section>
+          <div className={styles.header}>
+              <h1>Projects</h1>
+          </div>
+          <div className={styles.portfolio}>
+            <div className={styles.categories}>
+              <p>test</p>
+            </div>
+            <div className={styles.tags}>
+              <p>test</p>
+            </div>
+            <div className={styles.gallery}>
+              <p>gallery here</p>
+            </div>
+          </div>
+        </section>
+      </Wrapper>
     </Layout>
   )
 }
